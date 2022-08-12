8e2af7250c5af53ce819796228aff4473036532e
Gorayan <gorayan3838@gmail.com>
2022-08-12 09:11:48 +0900

    update とりあえずpublishするまで

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
-    kotlin("jvm").version("1.6.21")
+    kotlin("jvm").version("1.5.32")
+    `maven-publish`
 }
 
-group = "met.gorayan.mc"
-version = "0.0.0"
+val group = "net.gorayan.mc"
+val libraryVersion = "0.0.0"
+
+val publishRepository = "https://maven.pkg.github.com/Gorayan/paper-kotlin-utils"
 
 repositories {
 
@@ -18,4 +21,32 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+}
+
+publishing {
+    repositories {
+        maven {
+
+            name = "GitHubPackages"
+            url = uri(publishRepository)
+
+            credentials {
+
+                val props = org.jetbrains.kotlin.konan.properties.loadProperties("${projectDir}/private.properties")
+
+                username = props.getProperty("gpr.user") ?: System.getenv("USERNAME")
+                password = props.getProperty("gpr.key") ?: System.getenv("TOKEN")
+
+            }
+        }
+    }
+    publications {
+
+        register<MavenPublication>("gpr") {
+            groupId = group
+            artifactId = "paper-kotlin-utils"
+            version = libraryVersion
+            from(components["java"])
+        }
+    }
 }
